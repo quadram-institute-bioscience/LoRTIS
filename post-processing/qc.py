1cb3c68ffec7a85cf34919cfc5b217fdeddf7914
Martin Lott <47414207+martinclott@users.noreply.github.com>
2020-11-12 17:00:07 +0000

    Create qc.py

---
+++ post-processing/qc.py
@@ -0,0 +1,3 @@
+Check the plot file for large spikes (over 100,000 insertions) and report back to the user
+
+Consider how spread out the insertions are, do we have them evenly spread across the genome?
