04f46c1442107a99d55f3a6f027710910b61a8ef
Martin Lott <47414207+martinclott@users.noreply.github.com>
2020-11-19 11:23:45 +0000

    Add files via upload

---
+++ post-processing/combine-plot-files.py
@@ -0,0 +1,23 @@
+# Combine two plot files
+
+import sys
+
+filename1 = sys.argv[1]
+filename2 = sys.argv[2]
+
+plot1 = open(filename1)
+lines1=plot1.readlines()
+
+plot2 = open(filename2)
+lines2=plot2.readlines()
+
+if len(lines1)==len(lines2):
+    for i in range(0,len(lines1)):
+        field1 = lines1[i].rstrip().split(' ')
+        field2 = lines2[i].rstrip().split(' ')
+
+        positive = int(field1[0]) + int(field2[0])
+        negative = int(field1[1]) + int(field2[1])
+        print(str(positive) + ' ' + str(negative))
+else:
+    print('Error plot files are a different length, plot 1 is ' + str(len(lines1)) + ' whilst plot 2 is ' + str(len(lines2)))
