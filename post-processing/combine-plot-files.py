de788cd90d13bc680d412ef875d63c8617fa7675
Martin Lott <47414207+martinclott@users.noreply.github.com>
2020-11-24 11:31:11 +0000

    Add files via upload

--- post-processing/combine-plot-files.py
+++ post-processing/combine-plot-files.py
@@ -5,6 +5,9 @@ import sys
 filename1 = sys.argv[1]
 filename2 = sys.argv[2]
 
+mode='sum'
+if len(sys.argv) > 3: mode = sys.argv[3]
+
 plot1 = open(filename1)
 lines1=plot1.readlines()
 
@@ -16,8 +19,13 @@ if len(lines1)==len(lines2):
         field1 = lines1[i].rstrip().split(' ')
         field2 = lines2[i].rstrip().split(' ')
 
-        positive = int(field1[0]) + int(field2[0])
-        negative = int(field1[1]) + int(field2[1])
+        if mode=='sum':
+            positive = int(field1[0]) + int(field2[0])
+            negative = int(field1[1]) + int(field2[1])
+        elif mode=='min':
+            positive = min(int(field1[0]),int(field2[0]))
+            negative = min(int(field1[1]),int(field2[1]))
+            
         print(str(positive) + ' ' + str(negative))
 else:
     print('Error plot files are a different length, plot 1 is ' + str(len(lines1)) + ' whilst plot 2 is ' + str(len(lines2)))
