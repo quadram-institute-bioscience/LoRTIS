c6ed394729d8b149c2d4a636b96cb6e924dbcc8c
martinclott <martin.lott@quadram.ac.uk>
2021-11-12 13:14:53 +0000

    Convert a sam file to an insertion plot updated

--- scripts/sam_to_insert_plot.py
+++ scripts/sam_to_insert_plot.py
@@ -1,7 +1,18 @@
 import sys
 import pysam
 
+from Bio import SeqIO
+
 filename = sys.argv[1]
+reference_filename = sys.argv[2]
+
+contig_name_to_size=dict()
+
+reference_contigs = SeqIO.parse(open(reference_filename),'fasta')
+for reference_contig in reference_contigs:
+    contig_name_to_size[str(reference_contig.id)] = len(reference_contig.seq)
+
+#load reference_filename and compute the contig lengths
 
 forward_inserts_by_contig=dict()
 reverse_inserts_by_contig=dict()
@@ -10,11 +21,15 @@ reverse_inserts_by_contig=dict()
 
 samfile = pysam.AlignmentFile(filename)
 
+total_reads=0
+total_mapped=0
 
 for read in samfile:
+    total_reads = total_reads+1
 #    print(dir(read))
 
     if( not( read.is_unmapped ) ):   #if it's mapped
+        total_mapped = total_mapped+1
         contig_name = read.reference_name
         if contig_name not in forward_inserts_by_contig.keys(): forward_inserts_by_contig[contig_name]=dict()
         if contig_name not in reverse_inserts_by_contig.keys(): reverse_inserts_by_contig[contig_name]=dict()
@@ -91,21 +106,52 @@ for read in samfile:
 print(max(forward_inserts.keys()))
 print(max(reverse_inserts.keys()))
 
+insertion_sites_by_contig = dict()
+
 for contig_name in forward_inserts_by_contig:
+    if contig_name not in insertion_sites_by_contig.keys(): insertion_sites_by_contig[contig_name]=0
+    
     insert_plot_file = open(contig_name + '.insert_plot','w+')
     forward_inserts = forward_inserts_by_contig[contig_name]
     reverse_inserts = reverse_inserts_by_contig[contig_name]
                 
     for i in range(1,max(forward_inserts.keys())+1):
-        if i in forward_inserts.keys(): insert_plot_file.write(str(forward_inserts[i]))
+        insertion_site=False
+        if i in forward_inserts.keys():
+            insertion_site=True
+            insert_plot_file.write(str(forward_inserts[i]))
         else: insert_plot_file.write('0')
 
         insert_plot_file.write(' ')
             
-        if i in reverse_inserts.keys(): insert_plot_file.write(str(reverse_inserts[i]))
+        if i in reverse_inserts.keys():
+            insertion_site=True
+            insert_plot_file.write(str(reverse_inserts[i]))
         else: insert_plot_file.write('0')
+
+        if insertion_site: insertion_sites_by_contig[contig_name]=insertion_sites_by_contig[contig_name]+1
 
         insert_plot_file.write('\n')
 
+print("Output some stats here..\n")
+print("Total Reads : " + str(total_reads))
+print("Total Mapped : " + str(total_mapped))
+print("Percentage Mapped : " + str((100*total_mapped)/total_reads))
 
-print("Output some stats here..")
+total_uis=0
+total_contig_length=0
+for contig_name in insertion_sites_by_contig.keys():
+    print("Statistics for contig " + contig_name)
+    
+    print("Unique insertion sites " + str(insertion_sites_by_contig[contig_name]))
+    total_uis=total_uis+insertion_sites_by_contig[contig_name]
+    
+    print("Contig length " + str(contig_name_to_size[contig_name]))
+    total_contig_length=total_contig_length+contig_name_to_size[contig_name]
+    
+    print("Contig length / UIS " + str(contig_name_to_size[contig_name]/insertion_sites_by_contig[contig_name]))
+
+print("\n\nGrand total")
+print("Total Unique insertion sites " + str(total_uis))
+print("Total contig lengths " + str(total_contig_length))
+print("Total contig length / UIS " + str(total_contig_length/total_uis))
